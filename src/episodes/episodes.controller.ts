7a6f88fa83fccdf83556a096e94b8bc78fd833cf
jonathan mutakura <jmutakura@gmail.com>
2024-12-01 00:29:26 -0600

    added tests

--- src/episodes/episodes.controller.ts
+++ src/episodes/episodes.controller.ts
@@ -1,26 +1,30 @@
 import {Body, Controller, Get, Param, Post, Query} from '@nestjs/common';
+import {EpisodesService} from "./episodes.service";
+import {CreateEpisodeDto} from "./dto/create-episode.dto";
+import {ConfigService} from "../config/config.service";
 
 @Controller('episodes')
 export class EpisodesController {
+    constructor(private episodeService: EpisodesService, private readonly configService: ConfigService,) {}
     @Get()
     findAll(@Query('sort') sort: 'asc' | 'desc' = 'desc') {
-        return 'all episodes';
+        return this.episodeService.findAll(sort);
     }
 
     @Post()
-    create(@Body() input: any){
+    create(@Body() input: CreateEpisodeDto) {
         console.log(input);
-        return 'new episode';
+        return this.episodeService.create(input);
     }
 
     @Get('featured')
     findFeatured(){
-        return 'featured episodes';
+        return this.episodeService.findFeatured();
     }
 
     @Get(':id')
     findOne(@Param('id') id: string) {
-        return `episodes/${id}`;
+        return this.episodeService.findOne(id);
     }
 
 
