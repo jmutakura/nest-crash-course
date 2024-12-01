d0b55fe1ed94b7b51f337d524e626b828f3c47cf
jonathan mutakura <jmutakura@gmail.com>
2024-12-01 00:17:00 -0600

    added createEpisodeDto

--- src/episodes/episodes.controller.ts
+++ src/episodes/episodes.controller.ts
@@ -1,4 +1,29 @@
-import { Controller } from '@nestjs/common';
+import {Body, Controller, Get, Param, Post, Query} from '@nestjs/common';
 
 @Controller('episodes')
-export class EpisodesController {}
+export class EpisodesController {
+    @Get()
+    findAll(@Query('sort') sort: 'asc' | 'desc' = 'desc') {
+        return 'all episodes';
+    }
+
+    @Post()
+    create(@Body() input: any){
+        console.log(input);
+        return 'new episode';
+    }
+
+    @Get('featured')
+    findFeatured(){
+        return 'featured episodes';
+    }
+
+    @Get(':id')
+    findOne(@Param('id') id: string) {
+        return `episodes/${id}`;
+    }
+
+
+
+
+}
